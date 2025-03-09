42b04c9c5a0d68a80ab8e9c25d65f71a759c2f1b
ulxsth <ulxsth@gmail.com>
2025-03-09 09:46:12 +0900

    chore: 詳細画面にホームへのページ遷移を追加

--- src/components/pages/DiaryDetailPage.js
+++ src/components/pages/DiaryDetailPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DiaryDetailPage = () => {
   return (
     <div>
       <h1>Diary Detail</h1>
       <p>日記を表示するとこ</p>
+      <Link to={`/`}>Back to Home</Link>
     </div>
   );
 };
