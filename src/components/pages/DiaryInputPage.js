50189d5fc84fb374dd54c6a0d5a1d1b93d201048
ulxsth <ulxsth@gmail.com>
2025-03-09 10:21:18 +0900

    feat: マイクを実装してみた

--- src/components/pages/DiaryInputPage.js
+++ src/components/pages/DiaryInputPage.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const DiaryInputPage = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if(!browserSupportsSpeechRecognition) {
+    return <span>Speech Recognition is not supported in this browser.</span>;
+  }
+
   return (
     <div>
       <h1>Diary Input</h1>
-      <p>ここで音声を入力</p>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <p>{transcript}</p>
       <Link to="/diaries/:id/edit">Create</Link>
       <nav>
         <ul>
