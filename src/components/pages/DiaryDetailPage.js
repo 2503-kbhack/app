f164a31d422b4ab57c5cdf01d6888cda79cff3c5
Issa Nakatsuka <issanakatsuka0511@gmail.com>
2025-03-17 14:15:39 +0900

    日記のテーブルを作成して、Reactで表示されるようにした

--- src/components/pages/DiaryDetailPage.js
+++ src/components/pages/DiaryDetailPage.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React ,{useState,useEffect} from 'react';
+import { Link,useParams } from 'react-router-dom';
+import { fetchDiaries } from '../../api/fetchDiaries';
+const DiaryDetailPage = (props) => {
+  const { id } = useParams();
+  const [diary, setDiary] = useState([]);
 
-const DiaryDetailPage = () => {
+  
+  useEffect(() => {
+    // コンポーネントがマウントされた時に実行
+    const getDiary = async () => {
+      const data = await fetchDiaries({ id: id });
+      if (data) {
+        setDiary(data);
+      }
+    };
+
+    getDiary();
+  }, []);
+  console.log(diary[0]);
+  if (!diary[0]) {
+    return <div>読み込み中</div>;
+  }
   return (
     <div>
-      <h1>Diary Detail</h1>
-      <p>日記を表示するとこ</p>
+      <h1>{diary[0].title}</h1>
+      <p>{diary[0].contents}</p>
+      <Link to={`/diaries`}>Back to Diaries </Link>
+      <br />
       <Link to={`/`}>Back to Home</Link>
+      
+     
     </div>
   );
 };
