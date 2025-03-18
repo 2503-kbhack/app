643423aacd2f1a4e811bb0f0584dbf61cfe0d379
Shibutani <shibu1003@outlook.jp>
2025-03-18 23:28:23 +0900

    タイトル二個あったので消した

--- src/components/pages/AppHeader.js
+++ src/components/pages/AppHeader.js
@@ -8,8 +8,7 @@ function AppHeader() {
       <img src="/Bazaart_20250317_040552_859.png" alt="App Icon" className="app-icon" />
       {/* アプリ名 */}
       <img src="/title_logo.png" alt="App Icon" className="app-icon" />
-      
-      <h1 className="app-title">Coe-Nikki</h1>
+    
       <LogoutButton className="logout-button" /> {/* ③ここで props として className を渡す */}
     </header>
   );
